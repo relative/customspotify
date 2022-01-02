096cb8a177f0fb572eceb02ddf3ea664d6eaeb64
relative <hi@relative.im>
2022-01-02 12:29:44 -0500

    feat: create string Qt metaclass

--- src/startup.h
+++ src/startup.h
@@ -1,6 +1,11 @@
 #ifndef CUSTOMSPOTIFY_STARTUP_H
 #define CUSTOMSPOTIFY_STARTUP_H
 
+#include <string>
+#include <QObject>
+
+Q_DECLARE_METATYPE(std::string);
+
 void startup_create_directories();
 
 #endif //CUSTOMSPOTIFY_STARTUP_H
